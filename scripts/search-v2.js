edce865c0f40fe0d6a8c85e80087142d40c7f64c
Alex Bujenita <dekc89@gmail.com>
2019-04-12 14:01:54 +0100

    prettified and logs removed

--- scripts/search-v2.js
+++ scripts/search-v2.js
@@ -1,20 +1,16 @@
 function searchFunction() {
-  let input = document.getElementById('myinput');
+  let input = document.getElementById("myinput");
   let filter = input.value.toUpperCase();
-  let whole = document.getElementById('root');
-  let cardContainers = whole.getElementsByClassName('card');
+  let whole = document.getElementById("root");
+  let cardContainers = whole.getElementsByClassName("card");
 
   for (i = 0; i < cardContainers.length; i++) {
-    let title = cardContainers[i].getElementsByTagName('h1')[0]
+    let title = cardContainers[i].getElementsByTagName("h1")[0];
 
     if (title.textContent.toUpperCase().indexOf(filter) > -1) {
-      cardContainers[i].style.display = '';
-      console.log(title);
-    }
-
-    else {
-      cardContainers[i].style.display = 'none';
-      console.log(cardContainers[i]);
+      cardContainers[i].style.display = "";
+    } else {
+      cardContainers[i].style.display = "none";
     }
   }
 }
--- scripts/with-async-await.js
+++ scripts/with-async-await.js
@@ -1,4 +1,3 @@
-
 // TRY IP FETCH   //////////////////////////////////////////
 
 async function myIp() {
@@ -10,52 +9,54 @@ async function myIp() {
 // TRY IP FETCH   /////////////////////////////////////////
 
 const request = async () => {
-  const response = await fetch("https://data.cityofnewyork.us/resource/w7a6-9xrz.json");
+  const response = await fetch(
+    "https://data.cityofnewyork.us/resource/w7a6-9xrz.json"
+  );
   const obj = await response.json();
 
-  const root = document.getElementById('root');
+  const root = document.getElementById("root");
 
-  const srcBar = document.createElement('div');
-  srcBar.setAttribute('class', 'bar');
+  const srcBar = document.createElement("div");
+  srcBar.setAttribute("class", "bar");
 
-  const photo = document.createElement('img');
-  photo.src = './images/nyc.jpg';
+  const photo = document.createElement("img");
+  photo.src = "./images/nyc.jpg";
   photo.ondblclick = () => {
-    if(netAddress.clickCount !== 0) {
-      photo.style.filter = 'invert(0%)';
+    if (netAddress.clickCount !== 0) {
+      photo.style.filter = "invert(0%)";
       netAddress.clickCount = 0;
-      alert('Back to Earth, counter RESET!');
+      alert("Back to Earth, counter RESET!");
     } else {
-      photo.style.filter = 'invert(100%)';
+      photo.style.filter = "invert(100%)";
       netAddress.clickCount = 666;
-      alert('Welcome to Hell!');
+      alert("Welcome to Hell!");
     }
-  }
+  };
 
-  const netAddress = document.createElement('div'); //
-  netAddress.setAttribute('id', 'ip');
-  netAddress.setAttribute('class', 'bottom-right');
-  netAddress.innerHTML = 'Click to see your IP address';
+  const netAddress = document.createElement("div"); //
+  netAddress.setAttribute("id", "ip");
+  netAddress.setAttribute("class", "bottom-right");
+  netAddress.innerHTML = "Click to see your IP address";
   netAddress.clicked = false;
   netAddress.clickCount = 0;
   netAddress.onclick = async () => {
-
     // MORE "IF" TESTING
 
-    if(netAddress.clickCount === 6) {
+    if (netAddress.clickCount === 6) {
       alert("That's it mate! Search how to listen to your browser");
       window.location.replace("http://google.com");
     } else if (netAddress.clickCount === 4) {
-      alert("SLOW DOWN!!!")
+      alert("SLOW DOWN!!!");
     }
 
-    if(netAddress.clickCount === 666) {
-
+    if (netAddress.clickCount === 666) {
       setTimeout(alertSpam, 1000);
 
-      const call = () => window.location.replace('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/6e73a6b8-8288-40ca-9197-1474bb806cb8/d3hz2py-7d7efa0d-e58b-4b5c-97ed-2df59ce894e9.jpg');
+      const call = () =>
+        window.location.replace(
+          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/6e73a6b8-8288-40ca-9197-1474bb806cb8/d3hz2py-7d7efa0d-e58b-4b5c-97ed-2df59ce894e9.jpg"
+        );
       setTimeout(call, 7000);
-
     }
 
     //END OF TESTING
@@ -63,28 +64,28 @@ const request = async () => {
     if (!netAddress.clicked) {
       netAddress.clicked = true;
       netAddress.clickCount += 1;
-      netAddress.setAttribute('class', 'clicked');
-      netAddress.innerHTML = `Your IP is ${await myIp()}`
+      netAddress.setAttribute("class", "clicked");
+      netAddress.innerHTML = `Your IP is ${await myIp()}`;
     } else {
-      netAddress.setAttribute('class', 'bottom-right');
-      netAddress.innerHTML = 'Click to see your IP address';
+      netAddress.setAttribute("class", "bottom-right");
+      netAddress.innerHTML = "Click to see your IP address";
       netAddress.clickCount += 1;
       netAddress.clicked = false;
     }
-  }
+  };
 
-  const input = document.createElement('input');
-  input.setAttribute('type', 'text');
-  input.setAttribute('name', 'search');
-  input.setAttribute('value', '');
-  input.setAttribute('autocomplete', 'off');
-  input.setAttribute('id', 'myinput');
-  input.setAttribute('onkeyup', 'searchFunction()');
-  input.setAttribute('placeholder', 'Search a hospital...');
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "search");
+  input.setAttribute("value", "");
+  input.setAttribute("autocomplete", "off");
+  input.setAttribute("id", "myinput");
+  input.setAttribute("onkeyup", "searchFunction()");
+  input.setAttribute("placeholder", "Search a hospital...");
 
-  const container = document.createElement('div');
-  container.setAttribute('id', 'container');
-  container.setAttribute('class', 'container');
+  const container = document.createElement("div");
+  container.setAttribute("id", "container");
+  container.setAttribute("class", "container");
 
   root.appendChild(netAddress);
   root.appendChild(photo);
@@ -92,39 +93,34 @@ const request = async () => {
   srcBar.appendChild(input);
   root.appendChild(container);
 
-
   obj.forEach(hospital => {
     const card = document.createElement("div");
     card.setAttribute("id", "information");
     card.setAttribute("class", "card");
 
     const hospName = document.createElement("h1");
-    hospName.setAttribute("class", "name")
+    hospName.setAttribute("class", "name");
     hospName.textContent = hospital.facility_name;
 
-    const borough = document.createElement("p")
+    const borough = document.createElement("p");
     borough.textContent = `This hospital is located in ${hospital.borough}`;
 
     const phone = document.createElement("p");
     phone.textContent = `For appointments please call ${hospital.phone}`;
 
-    const facType = document.createElement('p');
+    const facType = document.createElement("p");
     facType.textContent = `This facility is a ${hospital.facility_type}`;
-    facType.style.display = 'none';
+    facType.style.display = "none";
 
-    const button = document.createElement('button');
-    button.innerHTML = 'Facility Type'
-    button.onclick = (event) => {
-  
-
-
-
-      if (event.target.nextElementSibling.style.display === 'inline-block') {
-        event.target.nextElementSibling.style.display = 'none';
-      } else if (event.target.nextElementSibling.style.display === 'none') {
-        event.target.nextElementSibling.style.display = 'inline-block';
+    const button = document.createElement("button");
+    button.innerHTML = "Facility Type";
+    button.onclick = event => {
+      if (event.target.nextElementSibling.style.display === "inline-block") {
+        event.target.nextElementSibling.style.display = "none";
+      } else if (event.target.nextElementSibling.style.display === "none") {
+        event.target.nextElementSibling.style.display = "inline-block";
       }
-    }
+    };
 
     container.appendChild(card);
 
@@ -133,8 +129,6 @@ const request = async () => {
     card.appendChild(phone);
     card.appendChild(button);
     card.appendChild(facType);
-  })
-
-
-}
+  });
+};
 request();
